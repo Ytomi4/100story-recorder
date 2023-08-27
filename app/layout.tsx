c806baf5968288dbc2177da792d31b2b0fc3ab12
Ytomi4 <kotirashuhei@gmail.com>
2023-08-27 16:05:17 +0900

    Change font to Shippori Mincho

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Shippori_Mincho } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const shippori_mincho = Shippori_Mincho({weight: "600", subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: '百物語Recorder',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={shippori_mincho.className}>{children}</body>
     </html>
   )
 }
