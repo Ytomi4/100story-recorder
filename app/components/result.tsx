5bd81fb052a5c597ed2de74483eacadbb37e894e
Ytomi4 <kotirashuhei@gmail.com>
2023-08-27 18:01:49 +0900

    Add download pathway for recorded text and summary on result page

--- app/components/result.tsx
+++ app/components/result.tsx
@@ -1,12 +1,41 @@
 import { useChat } from "ai/react"
 import Image from "next/image"
 import { getTitle, getSummary } from "../utils/chatTextParser"
+import { FaDownload } from 'react-icons/fa'
 
 export const Result = () => {
     const { messages } = useChat({id: 'main'})
 
     const title = messages.length > 1 ? <p>{getTitle(messages[messages.length - 1].content)}</p> : <p></p>
     const summary = messages.length > 1 ? <p>{getSummary(messages[messages.length -1].content)}</p> : <p></p>
+
+    console.log(messages)
+
+    const getCurrentTimestamp = () => {
+        const now = new Date();
+        const YYYY = now.getFullYear();
+        const MM = String(now.getMonth() + 1).padStart(2, '0');
+        const DD = String(now.getDate()).padStart(2, '0');
+        const HH = String(now.getHours()).padStart(2, '0');
+        const MIN = String(now.getMinutes()).padStart(2, '0');
+        const SS = String(now.getSeconds()).padStart(2, '0');
+        return `${YYYY}${MM}${DD}_${HH}${MIN}${SS}`;
+      };
+
+    const downloadFile = (filename: string, text: string) => {
+        const blob = new Blob([text], { type: 'text/plain' })
+        const url = URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = url
+        a.download = filename + '_' + getCurrentTimestamp() + '.txt'
+        a.click()
+        URL.revokeObjectURL(url)
+    }
+
+    const handleDownload = () => {
+        downloadFile('RecordedText', messages[0].content)
+        setTimeout(() => {downloadFile('Summary', messages[1].content)}, 1)
+    }
     
     return (
         <div className="flex flex-col items-center p-24">
@@ -15,12 +44,17 @@ export const Result = () => {
                     src = "/CandleCard.png"
                     alt = "Picture of candle"
                     layout = "fill"
-                    className="-z-10"
+                    className="-z-10 opacity-30"
                 />
-                <div className="absolute top-[5vh] px-[4vh] w-full text-center">{title}</div>
-                <div className="relative pt-[15vh] px-[4vh] ">{summary}</div>
-                <div className="absolute bottom-4 w-full text-right pr-10">名前です</div>
+                <div className="relative pt-8 px-[4vh] w-full text-center">{title}</div>
+                <div className="relative pt-5 px-[4vh] text-sm ">{summary}</div>
             </div>
+            <div className="relative flex mt-4">
+                <button className="px-3" onClick={handleDownload}>
+                    <FaDownload/>
+                </button>
+            </div>
+
         </div>
     )
 }
